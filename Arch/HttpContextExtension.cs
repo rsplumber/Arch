bb4e6a02738beeed4b36f04b8a27ecfa3bdc687d
s.kalami <s.kalami@tes.ir>
2023-01-10 13:42:19 +0330

    feature : HttpContextExtension and requestDispatcher method implemented.

---
+++ Arch/HttpContextExtension.cs
@@ -0,0 +1,17 @@
+﻿using Core;
+
+namespace Arch;
+
+public static class HttpContextExtension
+{
+    public static RequestInfo RequestDispatcher(this HttpContext httpContext)
+    {
+        return new RequestInfo
+        {
+            Body = httpContext.Request.Body,
+            Headers = httpContext.Request.Headers.ToDictionary(a => a.Key, a => string.Join(";", a.Value)),
+            Method = httpContext.Request.Method,
+            Path = httpContext.Request.Path
+        };
+    }
+}
