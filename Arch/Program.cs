56b721f7da4ded6e84c2c0fc3fd3969592262585
s.kalami <s.kalami@tes.ir>
2023-01-14 11:05:46 +0330

    refactor : Program.cs refactored.

--- Arch/Program.cs
+++ Arch/Program.cs
@@ -1,6 +1,35 @@
+using FastEndpoints;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.WebHost.UseKestrel();
+builder.WebHost.UseUrls("http://+:5228");
+
+
+builder.Services.AddCors();
+
+builder.Services.AddFastEndpoints();
+
 
 var app = builder.Build();
 
 
-app.Run();
+app.UseCors(b => b.AllowAnyHeader()
+    .AllowAnyMethod()
+    .SetIsOriginAllowed(_ => true)
+    .AllowCredentials());
+
+app.UseFastEndpoints(config =>
+{
+    config.Endpoints.RoutePrefix = "api";
+    config.Versioning.Prefix = "v";
+    config.Versioning.PrependToRoute = true;
+    config.Endpoints.Filter = ep => ep.EndpointTags?.Contains("hidden") is not true;
+});
+
+
+// if (app.Environment.IsDevelopment())
+// {
+// }
+
+
+await app.RunAsync();
