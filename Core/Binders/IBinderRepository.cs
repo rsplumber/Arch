3f3223e392b1b4d369831fb2c332948aee6e9eca
s.kalami <s.kalami@tes.ir>
2023-01-14 10:13:03 +0330

    feature : IBinderRepository and IServiceConfigRepository added.

---
+++ Core/Binders/IBinderRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace Core.Binders;
+
+public interface IBinderRepository
+{
+    Task AddAsync(Binder entity, CancellationToken cancellationToken = default);
+
+    Task UpdateAsync(Binder entity, CancellationToken cancellationToken = default);
+
+    Task DeleteAsync(Binder entity, CancellationToken cancellationToken = default);
+
+    Task<Binder?> FindAsync(string id, CancellationToken cancellationToken = default);
+}
---
+++ Core/ServiceConfigs/IServiceConfigRepository.cs
@@ -0,0 +1,14 @@
+﻿using Core.Binders;
+
+namespace Core.ServiceConfigs;
+
+public interface IServiceConfigRepository
+{
+    Task AddAsync(ServiceConfig entity, CancellationToken cancellationToken = default);
+
+    Task UpdateAsync(ServiceConfig entity, CancellationToken cancellationToken = default);
+
+    Task DeleteAsync(ServiceConfig entity, CancellationToken cancellationToken = default);
+
+    Task<Binder?> FindAsync(string id, CancellationToken cancellationToken = default);
+}
