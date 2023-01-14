daf0bb04479d56d57b29aecad058c5747c05e6c7
s.kalami <s.kalami@tes.ir>
2023-01-14 11:03:12 +0330

    feature : fastEndpoints added.

--- Arch/Program.cs
+++ Arch/Program.cs
@@ -1,19 +1,6 @@
-using System.Text.Json;
-using Arch;
-
 var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
 
-
-app.MapGet("/", () => "Hello World!");
-
-app.MapPost("/", async (HttpRequest request) =>
-{
-    var client = new HttpClient();
-    var req = request.RequestInfoExecutor();
-    var httpResponse = await client.PostAsJsonAsync("https://localhost:7023", req.Body);
-
-});
 
 app.Run();
