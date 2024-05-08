cad56444db22e5d39c04a22c09c736877f14dd13
s.kalami <s.kalami@tes.ir>
2024-05-08 10:24:47 +0330

    refactor : rate limit and CageRateLimit refactored.

---
+++ RateLimit.Configuration/ArchExtension.cs
@@ -0,0 +1,19 @@
+﻿using Arch.Configurations;
+
+namespace RateLimit.Configuration
+{
+    public static class ArchExtension
+    {
+
+        public static void UseRateLimit(this ArchOptions archOptions, Action<RateLimitOption> rateLimitOptions)
+        {
+            rateLimitOptions.Invoke(new RateLimitOption
+            {
+                Services = archOptions.Services,
+            });
+        }
+    }
+
+
+
+}
---
+++ RateLimit.Configuration/BeforeDispatchingOptionsExtension.cs
@@ -0,0 +1,15 @@
+﻿using Arch.Configurations;
+
+namespace RateLimit.Configuration
+{
+    public static class BeforeDispatchingOptionsExtension
+    {
+        public static void UseRateLimit(this BeforeDispatchingOptions beforeDispatchingOptions, Action<RateLimitExecutionOptions> options)
+        {
+            options.Invoke(new RateLimitExecutionOptions
+            {
+                ApplicationBuilder = beforeDispatchingOptions.ApplicationBuilder
+            });
+        }
+    }
+}
